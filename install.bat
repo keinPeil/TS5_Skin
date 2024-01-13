1cc6da2507676b1d4e5c1be28c5b809f34b59e00
keinPeil <66421939+keinPeil@users.noreply.github.com>
2024-01-13 18:01:38 +0100

    Fix install.bat

--- install.bat
+++ install.bat
@@ -1,4 +1,5 @@
 @ECHO OFF
 Echo "Beim installieren sollten 4 Dateien kopiert werden"
-copy de.keinpeil.teamspeak %appdata%\TeamSpeak\Default\extensions\de.keinpeil.teamspeak
+mkdir %appdata%\TeamSpeak\Default\extensions\de.keinpeil.teamspeak
+copy de.keinpeil.teamspeak %appdata%\TeamSpeak\Default\extensions\de.keinpeil.teamspeak\
 pause
